8ef2c52971d2132887d5b2964b91ec5a99b22376
Leqi Chen <chenl213@newschool.edu>
2022-04-29 14:03:34 -0400

    query

---
+++ assets/java-mobile.js
@@ -0,0 +1,89 @@
+function click_one_one() {
+	document.getElementById("open_1_1").style.display = "block";
+}
+
+function question_one_one() {
+	document.getElementById("selina_1_1").style.display = "block";
+	document.getElementById("down_1_1").style.display = "block";
+}
+
+
+
+function start_two_one() {
+	document.getElementById("open_2_1").style.display = "block";
+}
+
+function question_two_one() {
+	document.getElementById("selina_2_1").style.display = "block";
+	document.getElementById("down_2_1").style.display = "block";
+}
+
+
+
+function start_three_one() {
+	document.getElementById("open_3_1").style.display = "block";
+}
+
+function question_three_one() {
+	document.getElementById("selina_3_1").style.display = "block";
+	document.getElementById("down_3_1").style.display = "block";
+}
+
+
+
+function start_four_one() {
+	document.getElementById("open_4_1").style.display = "block";
+}
+
+function question_four_one() {
+	document.getElementById("selina_4_1").style.display = "block";
+}
+
+
+
+function start_five_one() {
+	document.getElementById("open_5_1").style.display = "block";
+}
+
+function question_five_one() {
+	document.getElementById("selina_5_1").style.display = "block";
+	document.getElementById("down_5_1").style.display = "block";
+}
+
+
+
+function start_six_one() {
+	document.getElementById("open_6_1").style.display = "block";
+}
+
+function question_six_one() {
+	document.getElementById("selina_6_1").style.display = "block";
+}
+
+
+
+function start_seven_one() {
+	document.getElementById("open_7_1").style.display = "block";
+}
+
+function question_seven_one() {
+	document.getElementById("selina_7_1").style.display = "block";
+	document.getElementById("down_7_1").style.display = "block";
+}
+
+
+
+function start_eight_one() {
+	document.getElementById("open_8_1").style.display = "block";
+}
+
+function question_eight_one() {
+	document.getElementById("selina_8_1").style.display = "block";
+	document.getElementById("final_print_1").style.display = "block";
+}
+
+
+
+function print_all() {
+	window.print()
+}
---
+++ assets/javascript.js
@@ -0,0 +1,131 @@
+
+function click_one() {
+	document.getElementById("question_form_1").style.display = "block";
+}
+
+function show_question_one() {
+	document.getElementById("open_1").style.display = "block";
+}
+
+function question_one() {
+	document.getElementById("answer_1").style.display = "block";
+}
+
+function answer_one() {
+	document.getElementById("selina_1").style.display = "block";
+	document.getElementById("down_1").style.display = "block";
+}
+
+
+
+function start_two() {
+	document.getElementById("start_2").style.display = "block";
+}
+
+function click_two() {
+	document.getElementById("question_form_2").style.display = "block";
+}
+
+function show_question_two() {
+	document.getElementById("open_2").style.display = "block";
+}
+
+
+function question_two() {
+	document.getElementById("answer_2").style.display = "block";
+}
+
+function answer_two() {
+	document.getElementById("selina_2").style.display = "block";
+	document.getElementById("down_2").style.display = "block";
+}
+
+
+
+function start_three() {
+	document.getElementById("open_3").style.display = "block";
+}
+
+
+function question_three() {
+	document.getElementById("selina_3").style.display = "block";
+	document.getElementById("down_3").style.display = "block";
+}
+
+
+
+function start_four() {
+	document.getElementById("start_4").style.display = "block";
+}
+
+function click_four() {
+	document.getElementById("open_4").style.display = "block";
+}
+
+function question_four() {
+	document.getElementById("start_5").style.display = "block";
+}
+
+function click_five() {
+	document.getElementById("answer_form_5").style.display = "block";
+}
+
+function show_answer_five() {
+	document.getElementById("selina_5").style.display = "block";
+	document.getElementById("down_5").style.display = "block";
+}
+
+
+
+function start_five() {
+	document.getElementById("selina_6").style.display = "block";
+	document.getElementById("down_6").style.display = "block";
+}
+
+
+
+function start_six() {
+	document.getElementById("start_6").style.display = "block";
+}
+
+function click_six() {
+	document.getElementById("open_7").style.display = "block";
+}
+
+function question_seven() {
+	document.getElementById("selina_7").style.display = "block";
+	document.getElementById("down_7").style.display = "block";
+}
+
+
+
+function start_seven() {
+	document.getElementById("start_7").style.display = "block";
+}
+
+function click_seven() {
+	document.getElementById("open_8").style.display = "block";
+}
+
+
+
+function question_eight() {
+	document.getElementById("start_8").style.display = "block";
+}
+
+function click_eight() {
+	document.getElementById("open_9").style.display = "block";
+}
+
+function question_nine() {
+	document.getElementById("selina_9").style.display = "block";
+	document.getElementById("final_print").style.display = "block";
+}
+
+
+
+
+function print_all() {
+	window.print()
+}
+
